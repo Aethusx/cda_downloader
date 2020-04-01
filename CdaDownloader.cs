9ef76a288351860e4bbc5951d9d3cd39713bf344
Aethusx <Aethusx@users.noreply.github.com>
2020-04-01 17:02:34 +0200

    Use HttpClient instead of WebClient.

--- CdaDownloader.cs
+++ CdaDownloader.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 static class CdaDownloader
 {
-    static WebClient web = new WebClient();
+    static HttpClient web = new HttpClient();
     static Regex regex_link = new Regex(@"https:\/\/www.cda.pl\/video\/([^\/\s]+)");
     static Regex regex_file = new Regex(@"""file"":""(.*?)(?:"")");
     static readonly string[] available_quality = { "360p", "480p", "720p", "1080p" };
 
     static CdaDownloader()
     {
-        web.Headers.Add("Referer", "https://www.cda.pl");
-        web.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0");
-        web.Headers.Add("Accept-Encoding", "identity");
+        web.DefaultRequestHeaders.Add("Referer", "https://www.cda.pl");
+        web.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0");
+        web.DefaultRequestHeaders.Add("Accept-Encoding", "identity");
     }
 
     /* cda robi przemagiczne rzeczy z tymi stringami */
@@ -22,7 +24,7 @@ static class CdaDownloader
     {
         string result = String.Empty;
 
-        foreach(char c in key)
+        foreach (char c in key)
             result += (c >= 33 && c <= 126) ? (char)(33 + ((c + 14) % 94)) : c;
 
         return (https) ? "https://" : "http://" + result + ".mp4";
@@ -30,6 +32,7 @@ static class CdaDownloader
 
     public static string GetVideoLink(string link, string quality = null, bool https = false)
     {
+
         if (link.EndsWith("/vfilm"))
             link = link.Substring(0, link.Length - 5);
 
@@ -46,7 +49,10 @@ static class CdaDownloader
             link = link + "?wersja=" + quality;
 
         /* mozna wyciagac to łopatologicznie ze z html tagów potem json objekt ale regex jest szybszy i krótszy :P :D */
-        Match match = regex_file.Match(web.DownloadString(link));
+        var task = Task.Run(() => web.GetAsync(link).Result.Content.ReadAsStringAsync());
+        task.Wait();
+
+        Match match = regex_file.Match(task.Result);
 
         if (match.Success && match.Groups.Count >= 2)
             return DecryptKey(Uri.UnescapeDataString(match.Groups[1].Value), https);
